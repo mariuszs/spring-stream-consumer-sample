f3698a5ba66ec6083ec226030287541ced592a4a
Mariusz Smykula <mariuszs@gmail.com>
2017-08-24 09:10:12 +0200

    fix json issue

--- src/main/java/com/example/streamconsumer/StreamConsumerApplication.java
+++ src/main/java/com/example/streamconsumer/StreamConsumerApplication.java
@@ -1,10 +1,14 @@
 package com.example.streamconsumer;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
@@ -17,5 +21,10 @@ public class StreamConsumerApplication {
     @StreamListener(Sink.INPUT)
     public void loggerSink(Foo foo) {
         System.out.println("Received: " + foo);
+    }
+
+    @Bean
+    public ParameterNamesModule parametersNameModule() {
+        return new ParameterNamesModule(JsonCreator.Mode.PROPERTIES);
     }
 }
