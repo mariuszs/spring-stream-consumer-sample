2d1f698eb2038bd38a7b769ba8ea0c5f186b8f10
Mariusz Smykula <mariuszs@gmail.com>
2017-08-27 13:46:49 +0200

    fix, one field pojo is not supported by jackson out-of-the-box :P

--- src/main/java/com/example/streamconsumer/Foo.java
+++ src/main/java/com/example/streamconsumer/Foo.java
@@ -3,10 +3,12 @@ package com.example.streamconsumer;
 public class Foo {
 
     private final String value;
+    private final String dummy;
 
     //FIXME: fails without @JsonProperty("value")
-    public Foo(String value) {
+    public Foo(String value, String dummy) {
         this.value = value;
+        this.dummy = dummy;
     }
 
     public String getValue() {
--- src/main/java/com/example/streamconsumer/StreamConsumerApplication.java
+++ src/main/java/com/example/streamconsumer/StreamConsumerApplication.java
@@ -1,30 +1,22 @@
 package com.example.streamconsumer;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableBinding(Sink.class)
 public class StreamConsumerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StreamConsumerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StreamConsumerApplication.class, args);
+	}
 
-    @StreamListener(Sink.INPUT)
-    public void loggerSink(Foo foo) {
-        System.out.println("Received: " + foo);
-    }
+	@StreamListener(Sink.INPUT)
+	public void loggerSink(Foo foo) {
+		System.out.println("Received: " + foo);
+	}
 
-    @Bean
-    public ParameterNamesModule parametersNameModule() {
-        return new ParameterNamesModule(JsonCreator.Mode.PROPERTIES);
-    }
 }
